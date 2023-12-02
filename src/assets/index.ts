22abd2aed0be580b8c5478ea7511db01befe9a26
siddhantprateek <siddhantprateek@gmail.com>
2023-12-02 21:37:02 +0530

    ft(assets): food images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,7 @@
 export { default as LOGO } from './logo.png';
+export { default as FOOD1 } from './plate.png';
+export { default as UNDERLINE } from './underline.png';
+export { default as ARROW } from './arrow.png';
+export { default as FRYING } from './fry.gif';
+export { default as FOOD2 } from './fish.png';
+export { default as FOOD3 } from './chole.png';
