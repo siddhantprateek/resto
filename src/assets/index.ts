d26b769ff351d5b8164ccfea636ade8350821642
siddhantprateek <siddhantprateek@gmail.com>
2023-12-02 23:56:28 +0530

    ft(assets): updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,3 +5,4 @@ export { default as ARROW } from './arrow.png';
 export { default as FRYING } from './fry.gif';
 export { default as FOOD2 } from './fish.png';
 export { default as FOOD3 } from './chole.png';
+export { default as CONCERT } from './Con.jpg';
