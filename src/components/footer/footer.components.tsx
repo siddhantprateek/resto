c7b91cbc6cfa2390ba852b4b078fd9183b305f4f
siddhantprateek <siddhantprateek@gmail.com>
2023-12-02 23:56:45 +0530

    ft(component): footer component updated

--- src/components/footer/footer.components.tsx
+++ src/components/footer/footer.components.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
+// Internals
+import './footer.styles.css';
+import { LOGO } from '../../assets';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="sub-footer-1">
+        <div className="left-footer-content">
+          <p>123 Gourmet Avenue,<br/> Culinary Metropolis</p>
+          <p>GET UPDATES ON FUN STUFF YOU <br/> PROBABLY WANT TO KNOW ABOUT<br/>  IN YOUR INBOX</p>
+          <input type="email" className='get-updated-email' placeholder='Email address' name="" id="" />
+        </div>
+        <div className="right-footer-content">
+          <ul className='footer-nav'>
+            <li>Menu</li>
+            <li>Sustainable Initiative</li>
+            <li>About Us</li>
+            <li>About Chef</li>
+            <li>Events</li>
+          </ul>
+        </div>
+      </div>
+      <div className="hr-line"></div>
+      <div className="sub-footer-2">
+        <div className="payment-options"></div>
+        <div className="socials"></div>
+        <div className="logo">
+          <img className='footer-logo' src={LOGO} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
