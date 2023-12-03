fb7138ed3830be163c198023f51dafae714a7b75
siddhantprateek <siddhantprateek@gmail.com>
2023-12-04 00:48:30 +0530

    ft(components): hero component done

--- src/components/hero/hero.components.tsx
+++ src/components/hero/hero.components.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { useInView  } from 'react-intersection-observer';
 // Internals 
 import './hero.styles.css';
+import { FRIES } from '../../assets';
 
 const Hero = () => {
   const { ref: heroRef, inView: heroView } = useInView({ threshold: 0, })
   return (
     <div className='hero'>
       <div className="hero-content" ref={heroRef}>
-        <h1 className={heroView ? "trans-from-left": ""}>It's not just <span className='red'>food</span> ,<br/> It's an <span className='blue'>experience</span>. 😇</h1>
+        <h1 className={heroView ? "trans-from-left": ""}>It's not just <span className='red liner'>food</span>,<br/>
+         <img className='fries' src={FRIES} alt="" />
+         It's an <span className='blue'>experience</span>. 😇</h1>
         <div className="secondary-content">
           <p className={`hero-content-2 ${heroView ? "trans-from-right-less": ""}`}>Relax Please, we've got you covered every day of the week.</p>
         </div>
