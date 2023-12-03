4099b29a041175b11999a0b712f194e5e0bf084c
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 16:23:34 +0530

    ft(event): event page animations

--- src/pages/events/events.pages.tsx
+++ src/pages/events/events.pages.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Internals
 import './events.styles.css';
 import { EventCard } from '../../components/common';
 import { Reservations } from '../../components';
+import { createMockServer } from '../../mirage/createMockServer';
+
+
+interface IEvent {
+  name: string
+  date: string 
+  description: string
+}
+
+// Mock Server 
+createMockServer()
 
 const Events = () => {
+  let [events, setEvents] = useState<IEvent[]>();
+
+  useEffect(() => {
+    fetch("https://restaurant.service/api/events")
+    .then((res) => res.json())
+    .then((json) => setEvents(json.events))
+  }, [])
+
   return (
     <div className='events-page'>
       <div className="event-header">
         <h1>EVENTS</h1>
       </div>
       <div className="listed-event-container">
-        <EventCard />
-        <EventCard />
+        {events?.map((event: IEvent) => (
+          <EventCard key={event.name} name={event.name} description={event.description} date={event.date} />
+        ))}
       </div>
       <Reservations />
     </div>
