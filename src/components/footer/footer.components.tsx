e46b0900a9c13a435160a0f4d8eef3339c5ce939
siddhantprateek <siddhantprateek@gmail.com>
2023-12-04 00:47:56 +0530

    ft(components): footer updated

--- src/components/footer/footer.components.tsx
+++ src/components/footer/footer.components.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useInView } from 'react-intersection-observer';
 
 // Internals
 import './footer.styles.css';
-import { LOGO } from '../../assets';
+import { FBICON, IGICON, LOGO, TWITTERICON, WEBSITEICON } from '../../assets';
 
 const Footer = () => {
+  const [ fbUrl, setfbUrl ] = useState<string>();
+  const [ igUrl, setigUrl ] = useState<string>();
+  const [ twtUrl, settwtUrl ] = useState<string>();
+  const [ website, setwebUrl ] = useState<string>();
+
+
+
   const { ref: footerRef, inView: footerView } = useInView({ threshold: 0, })
 
+  useEffect(() => {
+    fetch("https://restaurant.service/api/socials")
+    .then((res) => res.json())
+    .then((json) => {
+      setfbUrl(json.socials[0].facebook)
+      setigUrl(json.socials[0].instagram)
+      settwtUrl(json.socials[0].twitter)
+    })
+  }, [])
+
+
+  useEffect(() => {
+    fetch("https://restaurant.service/api/website")
+    .then((res) => res.json())
+    .then((json) => setwebUrl(json.website))
+  }, [])
   return (
     <div className={`footer ${footerView ? "trans-from-top": ""}`} ref={footerRef}>
       <div className="sub-footer-1">
@@ -28,9 +51,16 @@ const Footer = () => {
       </div>
       <div className="hr-line"></div>
       <div className="sub-footer-2">
-        <div className="payment-options"></div>
-        <div className="socials"></div>
+        <div className="payment-options">
+          <a target='__blank' href={website}><img className='social-icon' src={WEBSITEICON} alt="" /></a>
+        </div>
+        <div className="socials">
+          <a target='__blank' href={fbUrl}><img className='social-icon' src={FBICON} alt="" /></a>
+          <a target='__blank' href={igUrl}><img className='social-icon' src={IGICON} alt="" /></a>
+          <a target='__blank' href={twtUrl}><img className='social-icon' src={TWITTERICON} alt="" /></a>
+        </div>
         <div className="logo">
+          <p>Resto Inc. - Made by Siddhant</p>
           <img className='footer-logo' src={LOGO} alt="" />
         </div>
       </div>
