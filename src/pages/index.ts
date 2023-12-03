edcf813737755db884e122f54baf33860b2b67c6
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 12:12:54 +0530

    ft(pages): recipe profile need changes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as About } from './about/about.pages';
 // export { default as Restaurant } from './restaurant/restaurant.pages';
 export { default as Error404 } from './error/error404.pages';
 export { default as Menu } from './menu/menu.pages';
+export { default as Events } from './events/events.pages';
---
+++ src/pages/recipeProfile/recipeprofile.pages.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recipeprofile = () => {
+  return (
+    <div>Recipeprofile</div>
+  )
+}
+
+export default Recipeprofile
