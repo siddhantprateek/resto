53eeaa24600ec1c9ca6f5f33517830637b2e2ffa
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 10:02:05 +0530

    ft(components): reservations component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './header/header.components';
 export { default as Footer } from './footer/footer.components';
 export { default as Hero } from './hero/hero.components';
+export { default as Reservations } from './reservations/reservations.components';
