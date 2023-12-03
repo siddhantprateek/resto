c58bf4eda00f9f6f886a345d3ec919bed0669cea
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 10:01:51 +0530

    ft(pages): default exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { default as Home } from './home/home.pages';
 export { default as About } from './about/about.pages';
-export { default as Restaurant } from './restaurant/restaurant.pages';
+// export { default as Restaurant } from './restaurant/restaurant.pages';
 export { default as Error404 } from './error/error404.pages';
+export { default as Menu } from './menu/menu.pages';
