c762d3f925752ff073434d9143031ab2eb3ebb4f
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 12:11:02 +0530

    ft(common): fooditem component updated

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as AwardBadge } from './awardbadge/awardbadge.common';
 export { default as FoodItem } from './foodItem/foodItem.common';
 export { default as Testimony } from './testimony/testimony.common';
 export { default as Recipe } from './recipeItem/recipeItem.common';
+export { default as EventCard } from './eventcard/eventcard.common';
