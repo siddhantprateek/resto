3311a2e221bf8b6ffa523449fbdefb5253e4fa31
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 19:18:50 +0530

    ft(items): mirage items endpoint

--- src/mirage/createMockServer.ts
+++ src/mirage/createMockServer.ts
@@ -1,30 +1,61 @@
-import { createServer } from "miragejs";
+import { createServer, Model, Request, } from "miragejs";
+
+// Internals
 import data from '../data/restaurantData.json';
-
-
-
+import { Restaurant, MenuItem } from "./types";
 
 export const createMockServer = () => {
   createServer({
+    models: {
+      restaurant: Model.extend<Partial<Restaurant>>({}),
+    },
+
+    seeds(server) {
+      server.db.loadData({
+        restaurant: data,
+        initiatives: data.restaurant.sustainability.initiatives,
+        categories: data.restaurant.menu.categories,
+        items: [...data.restaurant.menu.categories[0].items,
+        ...data.restaurant.menu.categories[1].items,
+        ...data.restaurant.menu.categories[2].items,
+        ...data.restaurant.menu.seasonal_menu.items
+      ],
+        seasonal: data.restaurant.menu.seasonal_menu.items,
+      }
+    )},
+
     routes() {
       this.urlPrefix= "https://restaurant.service/api";
       this.get("/all", () => data.restaurant);
       this.get("/chef", () => {
         return { chef: data.restaurant.chef }
       })
+      
       this.get("/ambience", () => {
        return { ambience: data.restaurant.ambiance }
       });
+      
       this.get("/awards", () => data.restaurant.awards);
-      this.get("/food/menu", () => {
-        return { menu: data.restaurant.menu }
+      
+      this.get("/food/menu", (schema) => {
+        let items = schema.db.items
+        return { items: items }
       });
+
       this.get("/events", () => {
         return { events: data.restaurant.events.upcoming_events }
       });
-      // this.get("/food/menu", (schema, request: Request) => {
-      //   const { tags } = request.queryParams
-      // })
-    },
+      
+      this.get("/menu", (schema, request: Request) => {
+        const { season } = request.queryParams as { season: string };
+        console.log(season)
+        let allItems = schema.db.seasonal
+        console.log("All Seasonal: ", allItems)
+        const items = schema.db.seasonal.findBy({ seasonal_availability: season });
+        const items2 = schema.db.seasonal.where((item: any) => { item.seasonal_availability.includes(season)})
+        console.log(items2)
+        return { items: items || null };
+      });
+    }
   })
 }
---
+++ src/mirage/types.ts
@@ -0,0 +1,114 @@
+export interface ILocation {
+  address: string;
+  latitude: number;
+  longitude: number;
+}
+
+export interface IChef {
+  name: string;
+  bio: string;
+  signature_dish: string;
+}
+
+export interface IAward {
+  year: number;
+  organization: string;
+  award: string;
+}
+
+export interface IAmbiance {
+  description: string;
+}
+
+export interface IInitiative {
+  name: string;
+  description: string;
+}
+
+export interface ISustainability {
+  initiatives: IInitiative[];
+}
+
+export interface IUpcomingEvents {
+  name: string;
+  date: string; 
+  description: string;
+}
+
+export interface IEvents {
+  upcoming_events: IUpcomingEvents[]
+}
+
+export interface INutritionalInfo {
+  calories: number;
+  protein: number;
+  carbohydrates: number;
+  fat: number;
+}
+
+export interface MenuItem {
+  name: string;
+  description: string;
+  price: number;
+  ingredients: string[];
+  nutritional_info: INutritionalInfo
+  seasonal_availability: string[];
+}
+
+export interface Restaurant {
+  name: string;
+  location: ILocation;
+  chef: IChef;
+  awards: IAward[];
+  ambiance: IAmbiance;
+  sustainability: ISustainability
+  events: IEvents;
+  online_presence: {
+    website: string;
+    social_media: {
+      facebook: string;
+      instagram: string;
+      twitter: string;
+    };
+  };
+  menu: {
+    categories: {
+      name: string;
+      items: {
+        name: string;
+        description: string;
+        price: number;
+        ingredients: string[];
+        nutritional_info: {
+          calories: number;
+          protein: number;
+          carbohydrates: number;
+          fat: number;
+        };
+        seasonal_availability: string[];
+        customizable_options?: string[];
+      }[];
+    }[];
+    seasonal_menu: {
+      name: string;
+      items: {
+        name: string;
+        description: string;
+        price: number;
+        ingredients: string[];
+        nutritional_info: {
+          calories: number;
+          protein: number;
+          carbohydrates: number;
+          fat: number;
+        };
+        seasonal_availability: string[];
+      }[];
+    };
+  };
+  reviews: {
+    customer_name: string;
+    rating: number;
+    comment: string;
+  }[];
+}
