eef771d5a5c876cdd8055f6b381c864f10da89a9
siddhantprateek <siddhantprateek@gmail.com>
2023-12-03 09:59:44 +0530

    ft(assets): new assets added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,3 +6,17 @@ export { default as FRYING } from './fry.gif';
 export { default as FOOD2 } from './fish.png';
 export { default as FOOD3 } from './chole.png';
 export { default as CONCERT } from './Con.jpg';
+export { default as RES_MOCK1 } from './restaurant_mock1.jpg';
+
+// Mock Images
+export { default as MOCK1 } from './mock1.jpg';
+export { default as MOCK2 } from './mock2.jpg';
+export { default as MOCK3 } from './mock3.jpg';
+export { default as MOCK4 } from './mock4.jpg';
+export { default as MOCK5 } from './mock5.jpg';
+
+export { default as CHEF } from './chef.png';
+
+// Icons
+export { default as SEARCHICON } from './search-icon.png';
+export { default as RIGHTARW_ICON } from './right.png';
