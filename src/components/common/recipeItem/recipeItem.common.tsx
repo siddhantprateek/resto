32e1947d343b20791962162672b99d99fc74201c
siddhantprateek <siddhantprateek@gmail.com>
2023-12-04 08:41:15 +0530

    author: signature

--- src/components/common/recipeItem/recipeItem.common.tsx
+++ src/components/common/recipeItem/recipeItem.common.tsx
@@ -9,12 +9,13 @@ import './recipeItem.styles.css';
 import { MenuItem } from '../../../mirage/types';
 
 const URLS = [
-  'https://loremflickr.com/640/480/food?lock=7855588044177408',
-  'https://loremflickr.com/cache/resized/3092_2789914960_624f21f685_z_640_480_nofilter.jpg',
-  'https://loremflickr.com/cache/resized/65535_53045562761_ef0ba9722b_c_640_480_nofilter.jpg',
-  'https://loremflickr.com/cache/resized/65535_52976093903_3e1222f513_z_640_480_nofilter.jpg',
+  'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
   'https://www.archanaskitchen.com/images/archanaskitchen/1-Author/shaheen_ali/Stove_Top_Grilled__Smoked_Tandoori_Chicken.jpg',
-  'https://loremflickr.com/cache/resized/65535_52790943163_804ac4b806_b_640_480_nofilter.jpg'
+  'https://loremflickr.com/cache/resized/65535_53045562761_ef0ba9722b_c_640_480_nofilter.jpg',
+  'https://loremflickr.com/cache/resized/65535_52790943163_804ac4b806_b_640_480_nofilter.jpg',
 ]
 
 const RecipeItem = (props: MenuItem) => {
